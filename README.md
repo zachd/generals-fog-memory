25a0ab931b3190bd4468da2ce9bfc84489d3dc23
Zachary <hello@zach.ie>
2017-01-11 01:03:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+generals-crown-city-viewer
+=========
+
+A Chrome plugin that shows crowns and cities after they are hidden on [Generals.io](http://generals.io).
+
+#### Installation
+Download a copy of this repository to your computer. To do this from GitHub, click the green "Clone or Download" button and then "Download ZIP". Extract the ZIP file to a folder on your machine.
+
+Open Google Chrome and navigate to the Extensions screen (You can find this in More Tools -> Extensions). Ensure Developer Mode is checked, then click "Load unpacked extension..." and select the folder you extracted earlier.
---
+++ content_script.js
@@ -0,0 +1,48 @@
+var generals = {};
+var cities = [];
+
+// Check if in game
+function inGame(){
+  return document.getElementById('game-leaderboard');
+}
+
+// Turn function
+function turn(){
+  var map = document.getElementById('map');
+
+  // Search for generals
+  var found_gen = map.getElementsByClassName('general');
+  for (gen of found_gen){
+    var color = gen.className.substr(0, gen.className.indexOf(' '));
+    if(!generals[color])
+      generals[color] = gen;
+  }
+
+  // Search for cities
+  var found_city = map.getElementsByClassName('city');
+  for (city of found_city){
+    var color = city.className.substr(0, city.className.indexOf(' '));
+    if(cities.indexOf(city) === -1)
+      cities.push(city);
+  }
+
+  // Show generals
+  for (col of Object.keys(generals)){
+    var gen = generals[col];
+    if (!gen.className.includes("general"))
+      gen.className += " general " + col;
+  }
+
+  // Show cities
+  for (city of cities){
+    if (!city.className.includes("city"))
+      city.className = city.className.replace('obstacle', 'city');
+  }
+
+}
+
+turnInterval = setInterval(function() {
+  if (inGame()){
+    turn();
+  }
+}, 500);
